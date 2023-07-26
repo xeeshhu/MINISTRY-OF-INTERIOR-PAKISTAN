b3f784c031bf8a6f15034082b96297c5f440759b
xeeshhu <115435332+xeeshhu@users.noreply.github.com>
2023-07-26 16:21:28 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,3 +9,28 @@ function HomePage() {
 }
 
 export default HomePage;
+
+// Verified:
+// Issuing Authority: (MUHAMMAD ASAD MEHMOOD)
+// Section Officer (E-2)
+// Government of Pakistan
+
+// Allow For All Pakistan
+
+// User Information
+// Name: ABDUL REHMAN KHAN
+// Father Name: MUHAMMAD NAZEER
+// CNIC: 42501-9651061-7
+// City: KARACHI
+// Address: PHAN QAIDABAD BILMUKABIL SSP OFFICE NUMBER 1, BLOCK 36 AREA LAND MALIR KARACHI
+
+// Vehicles Information
+// Chasis No: CL049WLJ-400453
+// Engine No: 4D56-CU4106
+// Model: 1990
+// Make: JEEP
+// Car No: BC-0518
+// Colour: NOT DEFINED
+
+// Copyright
+// @2023 ministry of interior Pakistan Phone No: 051-9206660 Fax No: 051-9204193 Email ID: dslaw2@interior.gov.pk
