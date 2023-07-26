979ac4dc2bef542ce98e4e6ba5d97e2bc809d295
xeeshhu <115435332+xeeshhu@users.noreply.github.com>
2023-07-26 16:24:59 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,33 +4,37 @@ function HomePage() {
     <div>
       <h1>Hello, World!</h1>
       <p>This is my homepage content.</p>
+
+      {/* User Information */}
+      <h2>User Information</h2>
+      <p>
+        <strong>Name:</strong> ABDUL REHMAN KHAN<br />
+        <strong>Father Name:</strong> MUHAMMAD NAZEER<br />
+        <strong>CNIC:</strong> 42501-9651061-7<br />
+        <strong>City:</strong> KARACHI<br />
+        <strong>Address:</strong> PHAN QAIDABAD BILMUKABIL SSP OFFICE NUMBER 1, BLOCK 36 AREA LAND MALIR KARACHI
+      </p>
+
+      {/* Vehicles Information */}
+      <h2>Vehicles Information</h2>
+      <p>
+        <strong>Chasis No:</strong> CL049WLJ-400453<br />
+        <strong>Engine No:</strong> 4D56-CU4106<br />
+        <strong>Model:</strong> 1990<br />
+        <strong>Make:</strong> JEEP<br />
+        <strong>Car No:</strong> BC-0518<br />
+        <strong>Colour:</strong> NOT DEFINED
+      </p>
+
+      {/* Copyright */}
+      <p>
+        @2023 Ministry of Interior Pakistan<br />
+        Phone No: 051-9206660<br />
+        Fax No: 051-9204193<br />
+        Email ID: dslaw2@interior.gov.pk
+      </p>
     </div>
   );
 }
 
 export default HomePage;
-
-// Verified:
-// Issuing Authority: (MUHAMMAD ASAD MEHMOOD)
-// Section Officer (E-2)
-// Government of Pakistan
-
-// Allow For All Pakistan
-
-// User Information
-// Name: ABDUL REHMAN KHAN
-// Father Name: MUHAMMAD NAZEER
-// CNIC: 42501-9651061-7
-// City: KARACHI
-// Address: PHAN QAIDABAD BILMUKABIL SSP OFFICE NUMBER 1, BLOCK 36 AREA LAND MALIR KARACHI
-
-// Vehicles Information
-// Chasis No: CL049WLJ-400453
-// Engine No: 4D56-CU4106
-// Model: 1990
-// Make: JEEP
-// Car No: BC-0518
-// Colour: NOT DEFINED
-
-// Copyright
-// @2023 ministry of interior Pakistan Phone No: 051-9206660 Fax No: 051-9204193 Email ID: dslaw2@interior.gov.pk
