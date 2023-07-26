f0f40114515abbea053b94cce6606200858f7397
xeeshhu <115435332+xeeshhu@users.noreply.github.com>
2023-07-26 16:18:53 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Website Title</title>
-    <!-- Add any CSS or other meta tags you need -->
-</head>
-<body>
-    <!-- Your webpage content goes here -->
-    <header>
-        <h1>Welcome to Your Website</h1>
-    </header>
-    <nav>
-        <!-- Your navigation menu goes here -->
-    </nav>
-    <main>
-        <!-- Main content of your homepage goes here -->
-        <section>
-            <h2>About Us</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-        </section>
-    </main>
-    <footer>
-        <!-- Your footer content goes here -->
-    </footer>
-</body>
-</html>
+// pages/index.js
+function HomePage() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my homepage content.</p>
+    </div>
+  );
+}
+
+export default HomePage;
